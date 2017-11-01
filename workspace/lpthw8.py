46f4d29c60f89fc8cb3f8486a11aeac01435f160
DanielFranczak <dan.franczak@gmail.com>
2017-11-01 20:13:54 +0100

    Next exercises - esp. print, input. etc.

---
+++ workspace/lpthw8.py
@@ -0,0 +1,16 @@
+formatter = "{} {} {} {} {}"
+x = "It is really simple "
+print(formatter.format(1, 2, 3, 4, 5))
+print(formatter.format("one", "two", "three", "four", "five"))
+print(formatter.format('True2', False, True, False, True))
+print(formatter.format(formatter, formatter, formatter, formatter, x ))
+print(formatter.format(
+    "Life is not a pain",
+    "Life is miracle",
+    "We should celebrate it",
+    "No matter how deep in ass we're",
+    "So we need to tak it how it is"
+
+
+
+))
