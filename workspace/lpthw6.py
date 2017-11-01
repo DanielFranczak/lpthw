cf9687ffd400bfddf962ee7a71eda0f4d940e9b1
DanielFranczak <dan.franczak@gmail.com>
2017-11-01 14:09:14 +0100

    Made new exercises 7 and  8

--- workspace/lpthw6.py
+++ workspace/lpthw6.py
@@ -1,14 +1,14 @@
 types_of_people = 10
-#x = f"there are {types_of_people} types of people."
+x = f"there are {types_of_people} types of people."
 x2 = "there are {0} types of people".format(types_of_people)
 binary = "binary"
 do_not = "don't"
 #y = f"Those who know {binary} and those who {do_not}"
 y2 = "Those who know {0} and those who {1}".format(binary, do_not)
-print(x)
-print(2)
+print(x2)
+print(y2)
 print(f"I said: {x}")
-print(f"I also said: '{y}' ")
+print(f"I also said: '{y2}' ")
 hilarious = False
 joke_evaluation = "Isn't that joke so funny ?!{} "
 w = "This is the left sie of ..."
