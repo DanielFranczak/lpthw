c3cdeaa6b323a07ba4928a43412216c5d9ca17e8
DanielFranczak <dan.franczak@gmail.com>
2017-11-03 15:56:48 +0100

    New things

---
+++ workspace/lpthw15.py
@@ -0,0 +1,15 @@
+from sys import argv #import library from sys
+script, filename = argv #type script and give argument variable
+
+txt = open(filename) #responsible for open the file
+
+print(f"Here's your file {filename}:") #write on the screen name of opened file
+print(txt.read()) #write on the screen content of the file
+
+print("Type the filename again:") #shows another possibility to open the file
+file_again = input("> ") #ask us for write file name
+
+txt_again = open(file_again) # open the file
+
+print(txt_again.read()) #write content of the opened file on the screen
+
